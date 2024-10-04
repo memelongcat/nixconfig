f10287af7f3b6d7c8ff63bfdae0a7dba563d3651
memelongcat <krotovpeter87@outlook.com>
2024-10-05 01:31:34 +0700

    Add xrandr bash script

---
+++ xrandr.sh
@@ -0,0 +1,3 @@
+xrandr --newmode "1280x1024_75.00" 138.75  1280 1368 1504 1728  1024 1027 1034 1072 -hsync +vsync
+xrandr --addmode HDMI-1 1280x1024_75.00
+xrandr --output HDMI-1 --mode 1280x1024_75.00
