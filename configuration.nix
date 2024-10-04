9dd2c2b9dd74e5d77e5fd2ab3a6cfaf7ed541433
memelongcat <krotovpeter87@outlook.com>
2024-10-04 23:49:57 +0700

    Add new configs

---
+++ configuration.nix
@@ -0,0 +1,58 @@
+{
+	#BOOT
+	boot = {
+		loader = {
+			efi = {
+				canTouchEfiVariables = true;
+			};
+			grub = {
+				device = "nodev";
+				efiSupport = true;
+			};
+		};
+	};
+	#NETWORK
+	networking = {
+		hostName = "nixos"; #Hostname
+		networkmanager = { #Use Network Manager
+			enable = true;
+		};
+	};
+	#TIME
+	time = {
+		timeZone = "Asia/Krasnoyarsk";
+	};
+		#LOCALES
+	i18n = {
+		defaultLocale = "en_US.UTF-8";
+	};
+	#ENV
+	environment = {
+		systemPackages = with pkgs; [ #Общесистемные пакеты
+			neovim
+			git
+		];
+	};
+	#ENV
+	environment = {
+		systemPackages = with pkgs; [ #Общесистемные пакеты
+			btop 
+		];
+	};
+	#SECURTITY
+	security = {
+		sudo = {
+			enable = true;
+			extraConfig = ''
+				Defaults passwd_timeout=0 #отключение таймаута пароля
+				Defaults targetpw #Запрос пароля root
+			'';
+		};
+	};
+	#SERVICES
+	services = {
+		xserver = {
+            		enable = true;
+        	};
+    	};
+}
---
+++ disko.sh
@@ -0,0 +1,2 @@
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ./disko.nix
+
