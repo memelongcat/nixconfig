1decbabdc58852021e3d3b08ea54f31b54863ab2
memelongcat <krotovpeter87@outlook.com>
2024-10-05 05:02:43 +0700

    config fixes

--- configuration.nix
+++ configuration.nix
@@ -29,7 +29,7 @@
 		#LOCALES
 	i18n = {
 		defaultLocale = "en_US.UTF-8";
-		supportedLocales = [ "en_US.UTF-8" "ru_RU.UTF-8/UTF-8" ];
+		supportedLocales = [ "en_US.UTF-8/UTF-8" "ru_RU.UTF-8/UTF-8" ];
 	};
 	#ENV
 	environment = {
