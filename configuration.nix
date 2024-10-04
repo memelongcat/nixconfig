5abe07f0af64daaebcc7a7ed8a15b2516059918c
memelongcat <krotovpeter87@outlook.com>
2024-10-05 03:35:28 +0700

    config fixes

--- configuration.nix
+++ configuration.nix
@@ -90,6 +90,20 @@
 			packageOverrides = pkgs: {
 				unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {};
 			};
+			allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    				"steam"
+    				"steam-original"
+    				"steam-run"
+  			];
+		};
+	};
+	#PROGRAMS
+	programs = {
+		steam = {
+			enable = true;
+  			remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  			dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  			localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
 		};
 	};
 
