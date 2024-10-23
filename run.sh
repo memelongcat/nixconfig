944b4010f473447393f81efd17155f9c3b2a56c8
memelongcat <krotovpeter87@outlook.com>
2024-10-24 04:38:03 +0700

    config fixes

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-./ciadpi -i 127.0.0.1 -p 1080 --debug 2 --oob 1 --oob 2+s --mod-http rmspace --tlsrec 2+s
+./ciadpi -i 127.0.0.1 -p 1080 --debug 2 --split 2 --disorder 3 --fake -1 --ttl 5
 #./ciadpi -i 127.0.0.1 -p 8080 --fake -1 --ttl 5
